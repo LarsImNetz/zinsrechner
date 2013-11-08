a490daf6288ad204d8186709830fea5e79c3caec
Lars.im.Netz <java4fun@kaffeeschluerfer.com>
2013-11-08 22:15:53 +0100

    some changes, to make ZinsRechner work again

--- src/main/java/de/hypoport/einarbeitung/lars/ZinsRechnerPage.java
+++ src/main/java/de/hypoport/einarbeitung/lars/ZinsRechnerPage.java
@@ -14,32 +14,32 @@ public class ZinsRechnerPage extends WebPage {
 	public ZinsRechnerPage(PageParameters parameter) {
 		ZinsRechnerBean bean = new ZinsRechnerBean();
 		if (parameter != null) {
-			StringValue value = parameter.get("result");
-			if (value != null && !value.isEmpty()) {
-				bean.setResultat(Double.parseDouble(value.toString()));
-			}
 			StringValue miete = parameter.get("miete");
 			if (miete != null && !miete.isEmpty()) {
 				bean.setMiete(Double.parseDouble(miete.toString()));
 			}
 			StringValue extra = parameter.get("extra");
 			if (extra != null && !extra.isEmpty()) {
-				bean.setResultat(Double.parseDouble(extra.toString()));
+				bean.setSparen(Double.parseDouble(extra.toString()));
 			}
 			StringValue zins = parameter.get("zins");
 			if (zins != null && !zins.isEmpty()) {
-				bean.setResultat(Double.parseDouble(zins.toString()));
+				bean.setZins(Double.parseDouble(zins.toString()));
 			}
 			StringValue tilgung = parameter.get("tilgung");
 			if (tilgung != null && !tilgung.isEmpty()) {
-				bean.setResultat(Double.parseDouble(tilgung.toString()));
+				bean.setTilgung(Double.parseDouble(tilgung.toString()));
 			}
 			StringValue laufzeit = parameter.get("laufzeit");
 			if (laufzeit != null && !laufzeit.isEmpty()) {
-				bean.setResultat(Double.parseDouble(laufzeit.toString()));
+				bean.setLaufzeit(Integer.parseInt(laufzeit.toString()));
+			}
+			StringValue value = parameter.get("result");
+			if (value != null && !value.isEmpty()) {
+				bean.setResultat(Double.parseDouble(value.toString()));
 			}
 		}
-		beanModel = new Model<ZinsRechnerBean>();
+		beanModel = new Model<ZinsRechnerBean>(bean);
 		add(new ZinsRechnerPanel("rechner", beanModel));
 	}
 }
--- src/main/java/de/hypoport/einarbeitung/lars/ZinsRechnerPanel.java
+++ src/main/java/de/hypoport/einarbeitung/lars/ZinsRechnerPanel.java
@@ -2,14 +2,19 @@ package de.hypoport.einarbeitung.lars;
 
 import java.io.Serializable;
 
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.CssReferenceHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.internal.HtmlHeaderContainer;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.request.resource.CssResourceReference;
 import org.apache.wicket.validation.IErrorMessageSource;
 import org.apache.wicket.validation.IValidatable;
 import org.apache.wicket.validation.IValidationError;
@@ -32,19 +37,27 @@ public class ZinsRechnerPanel extends Panel {
 				double result = Math.floor(bean.getEndkapital());
 				bean.setResultat(result);
 				info("Berechnung durchgeführt");
-				PageParameters parameter = new PageParameters().add("result", result);
+
+				PageParameters parameter = new PageParameters();
+				parameter.add("miete", bean.getMiete());
+				parameter.add("extra", bean.getSparen());
+				parameter.add("zins", bean.getZins());
+				parameter.add("tilgung", bean.getTilgung());
+				parameter.add("laufzeit", bean.getLaufzeit());
+				parameter.add("result", bean.getResultat());
 				setResponsePage(ZinsRechnerPage.class, parameter);
-//				super.onSubmit();
-//				try {
-//					Thread.sleep(5000);
-//				} catch (InterruptedException e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				}
+				// super.onSubmit();
+				// try {
+				// Thread.sleep(5000);
+				// } catch (InterruptedException e) {
+				// // TODO Auto-generated catch block
+				// e.printStackTrace();
+				// }
 			}
 		};
 
-		TextField<Double> miete = new TextField<Double>("miete", new PropertyModel<Double>(beanModel, "miete"), Double.class);
+		TextField<Double> miete = new TextField<Double>("miete",
+				new PropertyModel<Double>(beanModel, "miete"), Double.class);
 		miete.setRequired(true);
 		miete.add(new IValidator<Double>() {
 
@@ -55,7 +68,8 @@ public class ZinsRechnerPanel extends Panel {
 					validatable.error(new IValidationError() {
 
 						@Override
-						public Serializable getErrorMessage(IErrorMessageSource messageSource) {
+						public Serializable getErrorMessage(
+								IErrorMessageSource messageSource) {
 							return "Die Miete sollte ein positiver Wert sein.";
 						}
 					});
@@ -63,16 +77,19 @@ public class ZinsRechnerPanel extends Panel {
 			}
 
 		});
-		ValidationIconPanel mieteFeedback = new ValidationIconPanel("mieteFeedback");
+		ValidationIconPanel mieteFeedback = new ValidationIconPanel(
+				"mieteFeedback");
 		form.add(mieteFeedback);
 		miete.add(new MarkIfValidationFailedBehavior(mieteFeedback));
 		miete.setOutputMarkupId(true);
 		form.add(miete);
 
-		TextField<Double> sparen = new TextField<Double>("sparen", new PropertyModel<Double>(beanModel, "sparen"), Double.class);
+		TextField<Double> sparen = new TextField<Double>("sparen",
+				new PropertyModel<Double>(beanModel, "sparen"), Double.class);
 		form.add(sparen);
 
-		TextField<Double> zins = new TextField<Double>("zins", new PropertyModel<Double>(beanModel, "zins"), Double.class);
+		TextField<Double> zins = new TextField<Double>("zins",
+				new PropertyModel<Double>(beanModel, "zins"), Double.class);
 		zins.add(new IValidator<Double>() {
 
 			@Override
@@ -82,7 +99,8 @@ public class ZinsRechnerPanel extends Panel {
 					validatable.error(new IValidationError() {
 
 						@Override
-						public Serializable getErrorMessage(IErrorMessageSource messageSource) {
+						public Serializable getErrorMessage(
+								IErrorMessageSource messageSource) {
 							return "Die Zins sollte z.Z. ein positiver Wert sein.";
 						}
 					});
@@ -90,12 +108,14 @@ public class ZinsRechnerPanel extends Panel {
 			}
 
 		});
-		ValidationIconPanel zinsFeedback = new ValidationIconPanel("zinsFeedback");
+		ValidationIconPanel zinsFeedback = new ValidationIconPanel(
+				"zinsFeedback");
 		form.add(zinsFeedback);
 		zins.add(new MarkIfValidationFailedBehavior(zinsFeedback));
 		form.add(zins);
 
-		TextField<Double> tilgung = new TextField<Double>("tilgung", new PropertyModel<Double>(beanModel, "tilgung"), Double.class);
+		TextField<Double> tilgung = new TextField<Double>("tilgung",
+				new PropertyModel<Double>(beanModel, "tilgung"), Double.class);
 		tilgung.add(new IValidator<Double>() {
 
 			@Override
@@ -105,7 +125,8 @@ public class ZinsRechnerPanel extends Panel {
 					validatable.error(new IValidationError() {
 
 						@Override
-						public Serializable getErrorMessage(IErrorMessageSource messageSource) {
+						public Serializable getErrorMessage(
+								IErrorMessageSource messageSource) {
 							return "Der Tilgungssatz sollte ein positiver Wert sein.";
 						}
 					});
@@ -113,15 +134,19 @@ public class ZinsRechnerPanel extends Panel {
 			}
 
 		});
-		ValidationIconPanel tilgungFeedback = new ValidationIconPanel("tilgungFeedback");
+		ValidationIconPanel tilgungFeedback = new ValidationIconPanel(
+				"tilgungFeedback");
 		form.add(tilgungFeedback);
 		tilgung.add(new MarkIfValidationFailedBehavior(tilgungFeedback));
 		tilgung.setOutputMarkupId(true);
 		form.add(tilgung);
 
-		TextField<Integer> laufzeit = new TextField<Integer>("laufzeit", new PropertyModel<Integer>(beanModel, "laufzeit"), Integer.class) {
+		TextField<Integer> laufzeit = new TextField<Integer>("laufzeit",
+				new PropertyModel<Integer>(beanModel, "laufzeit"),
+				Integer.class) {
 
-			// TODO: hier wäre ein kleiner Interpreter nett, der soetwas wie 12*j + 10 annehmen könnte.
+			// TODO: hier wäre ein kleiner Interpreter nett, der soetwas wie
+			// 12*j + 10 annehmen könnte.
 			public String getInput() {
 				String input = super.getInput();
 				int indexOfYear = input.indexOf('j');
@@ -132,20 +157,28 @@ public class ZinsRechnerPanel extends Panel {
 				}
 				return input;
 			};
-			//			protected Integer convertValue(String[] value) throws org.apache.wicket.util.convert.ConversionException {
-			//				Integer i = super.convertValue(value);
-			//				return i;
-			//			};
+			// protected Integer convertValue(String[] value) throws
+			// org.apache.wicket.util.convert.ConversionException {
+			// Integer i = super.convertValue(value);
+			// return i;
+			// };
 		};
 		laufzeit.setOutputMarkupId(true);
 		form.add(laufzeit);
 
-		TextField<Double> result = new TextField<Double>("resultat", new PropertyModel<Double>(beanModel, "resultat"), Double.class);
+		TextField<Double> result = new TextField<Double>("resultat",
+				new PropertyModel<Double>(beanModel, "resultat"), Double.class);
 		result.setOutputMarkupId(true);
 		form.add(result);
 
 		form.add(new FeedbackPanel("feedback"));
 
 		add(form);
+	}
+
+	@Override
+	public void renderHead(IHeaderResponse response) {
+		response.render(CssHeaderItem.forReference(new CssResourceReference(ZinsRechnerPage.class, "ZinsRechnerPage.css")));
+		super.renderHead(response);
 	}
 }
